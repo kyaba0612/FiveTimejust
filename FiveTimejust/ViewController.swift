a8f340a07c759f6ff56ef83fb4b09b932af05106
kyaba0612 <soutakawamura@icloud.com>
2020-10-04 22:54:37 +0900

    Initial Commit

---
+++ FiveTimejust/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FiveTimejust
+//
+//  Created by チップまん on 2020/10/04.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
